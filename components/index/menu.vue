c7a41fc86f9cf4c84853b7dd414ceaa7e3358597
fireairforce <1344492820@qq.com>
2019-10-21 18:52:30 +0800

    feat:完善了pages页面

---
+++ components/index/menu.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-menu">
+      <dl class="nav">
+          <dt>全部分类</dt>
+          <dd v-for="(item,idx) in menu" :key="idx">
+              <i :class="item.type" />{{item.title}}<span class="arrow" />
+          </dd>
+      </dl>
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+       return{
+           menu: [{
+               type:'food',
+               title:'美食'
+           },{
+               type: 'takeout',
+               title:'外卖'
+           }]
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
