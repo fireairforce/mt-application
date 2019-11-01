95d15694f9387d39d523fb8089e1a0b1c79ed477
fireairforce <1344492820@qq.com>
2019-11-01 20:02:56 +0800

    feat:新增注册用户接口

--- server/interface/users.js
+++ server/interface/users.js
@@ -5,3 +5,81 @@ import UserModel from "../dbs/models/users";
 import Passport from "./utils/passport";
 import Email from "../dbs/config";
 import axiso from "./utils/axios";
+import mongoose from "mongoose";
+
+let router = new Router({
+  prefix: "/users",
+});
+
+let Store = new Redis().client;
+const User = mongoose.model("UserModel");
+
+router.post("/signup", async (ctx) => {
+  // 这里要使用koa-bodyparser这个中间件去取值(把post的值映射到ctx.request.body上面去了)
+  const { username, password, email, code } = ctx.request.body;
+  if (code) {
+    const saveCode = await Store.hget(`nodemail: ${username}`, "code");
+    const saveExpire = await Store.hget(`nodemail: ${username}`, "expire");
+    if (code === saveCode) {
+      // 检查验证码是否过期
+      if (new Date().getTime() > saveExpire) {
+        ctx.body = {
+          code: -1,
+          msg: "验证码已过期，请重新尝试",
+        };
+        return false;
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: "请填写正确的验证码",
+      };
+    }
+  } else {
+    ctx.body = {
+      code: -1,
+      msg: "请填写验证码",
+    };
+  }
+  let user = await User.find({
+    username,
+  });
+  // 如果数据库里面已经存在这个用户了
+  if (user.length) {
+    ctx.body = {
+      code: -1,
+      msg: "该用户名已被注册了",
+    };
+    return;
+  }
+  // 如果数据库里面没有这个用户，那就相当于去注册一个了
+  let nuser = await User.create({
+    username,
+    password,
+    email,
+  });
+  if (nuser) {
+    let res = axios.post(`users/signup`, {
+      username,
+      password,
+    });
+    if (res.data && res.data.code === 0) {
+      ctx.body = {
+        code: 0,
+        msg: "注册成功",
+      };
+    } else {
+      ctx.body = {
+        code: -1,
+        msg: "error",
+      };
+    }
+  }
+  // 如果往库里写入数据发生了异常
+  else {
+    ctx.body = {
+      code: -1,
+      msg: "注册失败",
+    };
+  }
+});
--- server/interface/utils/passport.js
+++ server/interface/utils/passport.js
@@ -1,16 +1,18 @@
 import passport from "koa-passport";
 import LocalStrategy from "passport-local";
 import UserModel from "../../dbs/models/users";
-
+import mongoose from "mongoose";
+const User = mongoose.model('UserModel');
 // 使用passport这个中间价来处理一下登录这边的逻辑
 passport.use(
   new LocalStrategy(async function(username, password, done) {
     let where = {
       username,
     };
-    let result = await UserModel.findOne(where);
+    let result = await User.findOne(where);
     if (result) {
       //　使用数据库里面查询到的密码和输入的密码来做一个对比保证登录信息校验
+      // 如果数据库里面有这条信息
       if (result.password === password) {
         return done(null, result);
       } else {
@@ -22,14 +24,14 @@ passport.use(
   }),
 );
 
-// 序列化
-passport.serializeUser(function(user,done) {
-    done(null, user);
-})
+// 让用户每次登录进来的时候都通过session去进行一个自动化的验证(序列化)
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
 // 反序列化
-passport.deserializeUser(function(user,done) {
-    return done(null,user);
-})
+passport.deserializeUser(function(user, done) {
+  return done(null, user);
+});
 
 export default passport;
