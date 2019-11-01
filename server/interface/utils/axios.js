993ae4a0506029f432311e516f4181a2dc5a5276
fireairforce <1344492820@qq.com>
2019-11-01 12:02:18 +0800

    feat:新增axios函数配置

---
+++ server/interface/utils/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: `http://${process.env.HOST ||"localhost"}:${process.env.PORT || 3000}`,
+    // 设置超时时间
+    timeout: 1000,
+    headers: {
+   
+    }
+})
+
+export default instance;
