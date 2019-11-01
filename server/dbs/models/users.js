7a0c5b63301d95388e079f09471e81f0c6ddfffd
fireairforce <1344492820@qq.com>
2019-11-01 12:10:43 +0800

    feat:新增passport调试

--- server/dbs/models/users.js
+++ server/dbs/models/users.js
@@ -16,4 +16,4 @@ const UserSchema = new Schema({
     }
 })
 
-export default mongoose.model('User',UserSchema);
+export default mongoose.model('UserModel',UserSchema);
---
+++ server/interface/utils/passport.js
@@ -0,0 +1,35 @@
+import passport from "koa-passport";
+import LocalStrategy from "passport-local";
+import UserModel from "../../dbs/models/users";
+
+// 使用passport这个中间价来处理一下登录这边的逻辑
+passport.use(
+  new LocalStrategy(async function(username, password, done) {
+    let where = {
+      username,
+    };
+    let result = await UserModel.findOne(where);
+    if (result) {
+      //　使用数据库里面查询到的密码和输入的密码来做一个对比保证登录信息校验
+      if (result.password === password) {
+        return done(null, result);
+      } else {
+        return done(null, false, "密码错误");
+      }
+    } else {
+      return done(null, false, "用户不存在");
+    }
+  }),
+);
+
+// 序列化
+passport.serializeUser(function(user,done) {
+    done(null, user);
+})
+
+// 反序列化
+passport.deserializeUser(function(user,done) {
+    return done(null,user);
+})
+
+export default passport;
