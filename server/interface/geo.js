40e4157cca3e3482f0932b462f803d8bccfe82b3
fireairforce <1344492820@qq.com>
2019-11-21 22:05:09 +0800

    feat:完成城市位置的接口

--- server/interface/geo.js
+++ server/interface/geo.js
@@ -3,8 +3,22 @@ import axios from "./utils/axios";
 
 let router = new Router({ prefix: `/geo` });
 
-const sign = `abcd`;
-
-router.get(`getPosition`, async (ctx) => {});
+router.get(`getPosition`, async ctx => {
+  let {
+    status,
+    data: { province, city }
+  } = await axios.get(`http://cp-tools.cn/geo/getPosition`);
+  if (status === 200) {
+    ctx.body = {
+      province,
+      city
+    };
+  } else {
+    ctx.body = {
+      province: "",
+      city: ""
+    };
+  }
+});
 
 export default router;
