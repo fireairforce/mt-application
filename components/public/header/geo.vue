7235f0a46fc76270544a25a831e53bd3d125d125
fireairforce <1344492820@qq.com>
2019-11-22 13:03:52 +0800

    feat:从vuex中获取城市地址的数据

--- components/public/header/geo.vue
+++ components/public/header/geo.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="m-geo">
-    <i class="el-icon-location" />北京市
-    <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>[天门 黄冈 武汉]
+    <i class="el-icon-location"/>{{ $store.state.geo.position.city }}
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity">切换城市</nuxt-link>
+    [湖北 天门]
   </div>
 </template>
 
